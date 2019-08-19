e3d4d490c8d480e552f1e9261d9d8a288a9cb21a
HKTKJung <1181178674@qq.com>
2019-08-19 19:54:52 +0800

    add check cookies

--- src/main/java/com/jung/community/mapper/UserMapper.java
+++ src/main/java/com/jung/community/mapper/UserMapper.java
@@ -3,6 +3,8 @@ package com.jung.community.mapper;
 import com.jung.community.model.User;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 
 /**
  * Created by Jung on 2019/8/19 9:30
@@ -12,4 +14,6 @@ public interface UserMapper {
     @Insert("insert into user (name,account_id,token,gmt_create,gmt_modified) values (#{name},#{accountId},#{token},#{gmtCreate},#{gmtModified})")
     void insert(User user);
 
+    @Select("select * from user where token = #{token}")
+    User findByToken(@Param("token") String token);
 }
